3d5d53f37b8bba57e387b31ca41de579feef3ee8
TipsterTrickster <tipstertrickster13@gmail.com>
2018-12-23 11:23:52 -0500

    Added boolean field to form

--- competitions/models.py
+++ competitions/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Competition(models.Model):
     name = models.CharField(max_length=1000)
     decription = models.CharField(max_length=10000)
+    #add more events
+    three_by_three = models.BooleanField(default=True)
     date = models.DateField()
     def __str__(self):
         return self.name
