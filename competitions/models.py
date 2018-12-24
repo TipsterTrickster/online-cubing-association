cf9652fc00b1bf3d973838a896a8ec76eebd619d
TipsterTrickster <tipstertrickster13@gmail.com>
2018-12-24 15:42:50 -0500

    added more boolean fields

--- competitions/models.py
+++ competitions/models.py
@@ -5,6 +5,23 @@ class Competition(models.Model):
     decription = models.CharField(max_length=10000)
     #add more events
     three_by_three = models.BooleanField(default=True)
+    two_by_two = models.BooleanField(default=False)
+    four_by_four = models.BooleanField(default=False)
+    five_by_five = models.BooleanField(default=False)
+    six_by_six = models.BooleanField(default=False)
+    seven_by_seven = models.BooleanField(default=False)
+    three_blind = models.BooleanField(default=False)
+    FMC = models.BooleanField(default=False)
+    One_Handed = models.BooleanField(default=False)
+    Feet = models.BooleanField(default=False)
+    Clock = models.BooleanField(default=False)
+    Megaminx = models.BooleanField(default=False)
+    Pyraminx = models.BooleanField(default=False)
+    Skewb = models.BooleanField(default=False)
+    Square_1 = models.BooleanField(default=False)
+    four_blind = models.BooleanField(default=False)
+    five_blind = models.BooleanField(default=False)
+    multi_blind = models.BooleanField(default=False)
     date = models.DateField()
     def __str__(self):
         return self.name
